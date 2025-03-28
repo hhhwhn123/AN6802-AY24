75e77d9626ab08c494bdba0d698343b03b1f5058
hhhwhn123 <WANG2201@e.ntu.edu.sg>
2025-03-28 12:45:35 +0000

    8(w4)

---
+++ static/foodexp.js
@@ -0,0 +1,5 @@
+function predict_foodexp() {
+    const salary = document.getElementById("salary").value;
+    console.log(salary);
+    document.getElementById('foodexp').innerText = "Predicted Food Exp: " + ((salary*0.4851)+147.4)
+}
